2f7f64ba573b182b64936a2003a203db7d20166b
Andy Vo <avv03835@uga.edu>
2023-05-03 18:13:33 -0400

    added vars

--- src/main/java/cs1302/api/ApiApp.java
+++ src/main/java/cs1302/api/ApiApp.java
@@ -8,14 +8,68 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ProgressBar;
 
 /**
  * REPLACE WITH NON-SHOUTING DESCRIPTION OF YOUR APP.
  */
 public class ApiApp extends Application {
     Stage stage;
-    Scene scene;
-    VBox root;
+
+    Scene startScene;
+    VBox startRoot;
+    HBox searchPane;
+    Label searchLabel;
+    TextField searchField;
+    Button searchButton;
+    ScrollPane startScroll;
+    VBox scrollVBox;
+    HBox[] dishArray;
+    ImageView[] imageArray;
+    TextFlow[] descArray;
+    Button recipeButton;
+    HBox progressPane;
+    ProgressBar progressBar;
+    Button setLocButton;
+
+    Scene setLocScene;
+    VBox setLocRoot;
+    HBox setLocBackPane;
+    Button setLocBack;
+    HBox setLocPane;
+    Label setLocLabel;
+    TextField cityField;
+
+    Scene recipeScene;
+    VBox recipeRoot;
+    HBox recipeBackPane;
+    Button recipeBack;
+    TextFlow recipeFlow;
+    HBox recipePane;
+    VBox ingrPane;
+    ImageView recipeImage;
+    ScrollPane ingrScroll;
+    VBox ingrScrollPane;
+    Hyperlink[] ingrList;
+    TextFlow instrFlow;
+
+    Scene ingrScene;
+    VBox ingrBackPane;
+    Button ingrBack;
+    TextFlow ingrFlow;
+    HBox[] ingrPaneList;
+    ImageView[] ingrImages;
+    TextFlow[] ingrNames;
+    TextFlow[] ingrPrices;
 
     /**
      * Constructs an {@code ApiApp} object. This default (i.e., no argument)
@@ -24,6 +78,12 @@ public class ApiApp extends Application {
     public ApiApp() {
         root = new VBox();
     } // ApiApp
+
+    /** {@inheritDoc} */
+    @Override
+    public void init() {
+
+    }
 
     /** {@inheritDoc} */
     @Override
